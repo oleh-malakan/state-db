21453dc170fd0e809673eb1f82ad025b937b2e66
Oleh Malakan <olehdevt@gmail.com>
2024-01-03 13:55:34 +0200

    update

--- db.go
+++ db.go
@@ -1,5 +1,9 @@
 package state
 
+import (
+	"time"
+)
+
 func Open(filePath string) (*Entity, error) {
 	return &Entity{}, nil
 }
@@ -32,6 +36,34 @@ func (e *Entity) Last() (ok bool) {
 
 func (e *Entity) Data(fieldName ...string) *Data {
 	return &Data{}
+}
+
+func (e *Entity) Goto(link *Link) (ok bool) {
+	return
+}
+
+func (e *Entity) Link() *Link {
+	return &Link{}
+}
+
+func (e *Entity) Cut() *Clipboard {
+	return &Clipboard{}
+}
+
+func (e *Entity) Copy() *Clipboard {
+	return &Clipboard{}
+}
+
+func (e *Entity) Paste(clipboard ...Clipboard) error {
+	return nil
+}
+
+func (e *Entity) New(template *Template) error {
+	return nil
+}
+
+func (e *Entity) Template() *Template {
+	return &Template{}
 }
 
 func (e *Entity) Begin() (*Tx, error) {
@@ -76,6 +108,34 @@ func (t *Tx) Data(fieldName ...string) *Data {
 	return &Data{}
 }
 
+func (t *Tx) Goto(link *Link) (ok bool) {
+	return
+}
+
+func (t *Tx) Link() *Link {
+	return &Link{}
+}
+
+func (t *Tx) Cut() *Clipboard {
+	return &Clipboard{}
+}
+
+func (t *Tx) Copy() *Clipboard {
+	return &Clipboard{}
+}
+
+func (t *Tx) Paste(clipboard ...Clipboard) error {
+	return nil
+}
+
+func (t *Tx) New(template *Template) error {
+	return nil
+}
+
+func (t *Tx) Template() *Template {
+	return &Template{}
+}
+
 func (t *Tx) Commit() error {
 	return nil
 }
@@ -86,10 +146,44 @@ func (t *Tx) Rollback() error {
 
 type Data struct{}
 
-func (d *Data) Scan(v ...any) error {
+func (d *Data) Scan(dest ...any) error {
+	for _, d := range dest {
+		switch v := d.(type) {
+		case *Entity:
+			*v = Entity{}
+		case *Link:
+			*v = Link{}
+		case *Bookmarks:
+			*v = Bookmarks{}
+		case *Template:
+			*v = Template{}
+		}
+	}
 	return nil
 }
 
-func (d *Data) Update(v ...any) error {
+func (d *Data) Update(args ...any) error {
 	return nil
 }
+
+type Link struct{}
+
+type Bookmarks struct{}
+
+func (b *Bookmarks) Set(key string, link *Link) {}
+
+func (b *Bookmarks) SetTTL(key string, link *Link, ttl time.Duration) {}
+
+func (b *Bookmarks) Get(key string) *Link {
+	return &Link{}
+}
+
+func (b *Bookmarks) Contains(key string) bool {
+	return false
+}
+
+func (b *Bookmarks) Delete(key string) {}
+
+type Clipboard struct{}
+
+type Template struct{}
